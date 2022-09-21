7c60986372346c9a863142438b663a794e3f2557
Lali <Lali@DESKTOP-3F230QF>
2022-09-21 19:39:11 +0200

    Add project files.

---
+++ Teglalap/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Teglalap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void szamolj_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(a_oldal.Text), b = double.Parse(b_oldal.Text);
+            double K = (a + b) * 2, T = a * b;
+
+            Kerulet.Text = K.ToString() + " cm";
+            Terulet.Text = T.ToString() + " cm2";
+        }
+    }
+}
